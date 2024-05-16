818439fe97dbc58a0774a28d3a9084501d48da59
Bruno Sales <salesdasilvabruno@gmail.com>
2024-05-16 12:57:58 -0300

    criado a classe ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next(); 
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next(); 
+
+        System.out.println("Digite sua agência");
+        String agencia = scanner.next(); 
+
+        System.out.println("Digite sua conta");
+        int numero = scanner.nextInt(); 
+
+        System.out.println("Digite seu saldo");
+        double saldo = scanner.nextDouble(); 
+        
+       
+
+        System.out.println("Ola " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque." );
     }
 }
